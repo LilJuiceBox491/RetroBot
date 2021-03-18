6007adcd4251e75f91cb71a6784dcc7cd8929330
brazzmo <43312020+brazzmo@users.noreply.github.com>
2021-03-18 10:22:51 -0700

    add lastReset property to be updated on each restart

--- core/events/ready.js
+++ core/events/ready.js
@@ -1,5 +1,6 @@
 const statusInterval = require('../util/statusInterval');
 const Help = require('../Handlers/Help');
+const moment = require('moment');
 
 // eslint-disable-next-line no-unused-vars
 const purgeCachedUsers = require('../util/purgeCachedUsers');
@@ -13,6 +14,7 @@ module.exports = {
         Help(client);
 
         client.log.ready(`${client.user.username} is now awake in ${client.guilds.cache.size} servers!`);
+        client.lastReset = moment().format('MM/DD/YYYY HH:mm:ss');
 
         //Cycle Status
         statusInterval(client);
