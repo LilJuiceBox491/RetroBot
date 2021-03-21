2402a322a206e7ea21b31ae076e34502b0ec8ffb
brazzmo <43312020+brazzmo@users.noreply.github.com>
2021-03-21 16:19:29 -0700

    minor statusInterval change (#5)

--- core/util/statusInterval.js
+++ core/util/statusInterval.js
@@ -1,6 +1,7 @@
 const statuses = require('../constants/statuses');
 
 module.exports = (client) => {
+    client.user.setPresence({ activity: { type: 'LISTENING', name: 'startup events...'}, status: 'online' });
     setInterval(() => {
         const rand = Math.floor(Math.random() * statuses.length);
         const status = statuses[rand];
