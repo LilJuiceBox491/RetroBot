079d560c7371007551ed3194e9a6d38a79d86d4b
brazzmo <43312020+brazzmo@users.noreply.github.com>
2021-03-22 14:50:52 -0700

    Create kick.js

---
+++ core/commands/kick.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+    config: {
+        name: 'kick',
+        aliases: [],
+        permissions: ['KICK_MEMBERS'],
+        clientPermissions: ['KICK_MEMBERS'],
+        ownerOnly: true,
+        group: 'mod',
+        description: '*Coming Soon...*',
+        usage: 'kick <mention|id> [reason]'
+    },
+    run: (client, message, args) => {
+        return;
+    }
+}
