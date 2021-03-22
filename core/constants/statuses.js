fd8bd266f939b7f373c42ff0e4645ae157fdacc6
brazzmo <43312020+brazzmo@users.noreply.github.com>
2021-03-21 18:01:37 -0700

    add support command (#8)
    * add support command
    
    * Update statuses.js

--- core/constants/statuses.js
+++ core/constants/statuses.js
@@ -1,4 +1,5 @@
 module.exports = [
     { name: 'for {prefix}help', type: 'WATCHING' },
-    { name: 'for {prefix}info', type: 'WATCHING' }
+    { name: 'for {prefix}info', type: 'WATCHING' },
+    { name: 'for {prefix}support', type: 'WATCHING' }
 ];
