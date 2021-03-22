51eb9b61cdfe98d4c0f1738219f56b89077bfd5c
brazzmo <43312020+brazzmo@users.noreply.github.com>
2021-03-22 09:46:03 -0700

    Update moment format and timezone

--- core/events/ready.js
+++ core/events/ready.js
@@ -1,6 +1,6 @@
 const statusInterval = require('../util/statusInterval');
 const Help = require('../Handlers/Help');
-const moment = require('moment');
+const moment = require('moment-timezone');
 
 // eslint-disable-next-line no-unused-vars
 const purgeCachedUsers = require('../util/purgeCachedUsers');
@@ -14,7 +14,7 @@ module.exports = {
         Help(client);
 
         client.log.ready(`${client.user.username} is now awake in ${client.guilds.cache.size} servers!`);
-        client.lastReset = moment().format('MM/DD/YYYY HH:mm:ss');
+        client.lastReset = moment().tz('America/Los_Angeles').format('MM/DD/YYYY [at] hh:mm a [(PDT)]');
 
         //Cycle Status
         statusInterval(client);
