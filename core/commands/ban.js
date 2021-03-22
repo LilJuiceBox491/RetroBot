23ace2c1e081b18b7ef8352d996ccfaff7ca852b
brazzmo <43312020+brazzmo@users.noreply.github.com>
2021-03-22 14:50:50 -0700

    Create ban.js

---
+++ core/commands/ban.js
@@ -0,0 +1,15 @@
+module.exports = {
+    config: {
+        name: 'ban',
+        aliases: [],
+        permissions: ['BAN_MEMBERS'],
+        clientPermissions: ['BAN_MEMBERS'],
+        ownerOnly: true,
+        group: 'mod',
+        description: '*Coming Soon...*',
+        usage: 'ban <mention|id> [reason]'
+    },
+    run: (client, message, args) => {
+        return;
+    }
+}
