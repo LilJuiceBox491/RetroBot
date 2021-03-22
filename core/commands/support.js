9b826d3e6bf4dbc74adf9d1e681c7df2ad74dadf
brazzmo <43312020+brazzmo@users.noreply.github.com>
2021-03-21 17:59:36 -0700

    add support command

---
+++ core/commands/support.js
@@ -0,0 +1,25 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    config: {
+        name: 'support',
+        aliases: [], 
+        permissions: [], 
+        clientPermissions: [], 
+        group: 'util',
+        description: 'Replies with a link to the support server.',
+        usage: 'support'
+    },
+    // eslint-disable-next-line no-unused-vars
+    run: (client, message, args) => {
+        const e = new MessageEmbed()
+        .setAuthor(message.author.tag, message.author.avatarURL())
+        .setColor(client.config.colors.commands)
+        .addFields(
+            {name: 'Server:', value: `Please click [here](https://discord.gg/8Pfj8GXRvk) to join the support server!`},
+            {name: 'Info:', value: `For other important information, please refer to the ${client.config.prefix}info command.`}
+        );
+
+        message.channel.send(e);
+    }
+}
