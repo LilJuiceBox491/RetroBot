543efca13535c447bac2c28b5ac799f1fdc5fb96
brazzmo <43312020+brazzmo@users.noreply.github.com>
2021-03-21 23:33:12 -0700

    add `status` property

--- core/constants/statuses.js
+++ core/constants/statuses.js
@@ -1,5 +1,5 @@
 module.exports = [
-    { name: 'for {prefix}help', type: 'WATCHING' },
-    { name: 'for {prefix}info', type: 'WATCHING' },
-    { name: 'for {prefix}support', type: 'WATCHING' }
+    { name: 'for {prefix}help', type: 'WATCHING', status: 'online' },
+    { name: 'for {prefix}info', type: 'WATCHING', status: 'online' },
+    { name: 'for {prefix}support', type: 'WATCHING', status: 'online' }
 ];
